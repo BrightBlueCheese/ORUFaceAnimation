c613bcae2514cd32b40fb63178fdc417a1b1c320
DarkBlueCheese <dolazy7@gmail.com>
2023-07-06 09:34:37 -0500

    added ipynb and source files

---
+++ README.md
@@ -0,0 +1,21 @@
+# ORU Face Animation
+This code was built based on Vox dataset format .
+This code was built on the followings.
+- CoquiTTS
+- https://github.com/yoyo-nb/Thin-Plate-Spline-Motion-Model
+- https://github.com/Rudrabha/Wav2Lip
+
+# Source Video Guides
+- Should take the front face video.
+- The code will automatically detect and preprocess the source video. You don't have to worry about the resolution or the size of the video.
+
+# Limitation
+- The source image preprocessing stage has not been implemented.
+- Should have to prepare the image with vox format for the best performance.
+
+# Addition
+- Work through the FaceSyncProto.ipynb file.
+- The code already has the source image and video file for the test.
+
+
+Thanks
---
+++ rough_packages_saved.py
@@ -0,0 +1,99 @@
+import face_recognition
+
+
+
+def frame_definer(target_frame):
+    # Convert the BGR frame to RGB for displaying with plt
+    face_locations = face_recognition.face_locations(target_frame, model="cnn")
+
+    # Get the image shape
+    image_height, image_width, _ = target_frame.shape
+
+    # Iterate over the detected faces
+    for face_location in face_locations:
+        # Extract the coordinates of the face bounding box
+        top, right, bottom, left = face_location
+
+        # Scale the coordinates to match the image shape
+        scaled_top = top / image_height
+        scaled_right = right / image_width
+        scaled_bottom = bottom / image_height
+        scaled_left = left / image_width
+
+        # print(scaled_top, scaled_right, scaled_bottom, scaled_left)
+
+        # print((scaled_left + scaled_right) / 2)
+
+        # Calculate center coordinates
+        center_x = (scaled_left + scaled_right) / 2
+        center_y = (scaled_top + scaled_bottom) / 2
+
+        # Expand rectangle with certain ratio
+        expand_width = abs(center_x - scaled_left) * 1.0
+        expand_height = abs(center_y - scaled_top) * 1.0 # not use
+        expand_top = abs(center_y - scaled_top) * 1.0
+        expand_bottom = abs(center_y - scaled_top) * 1.0
+
+        # Crop the face region from the original image
+        back_scaled_top = scaled_top - expand_top
+        back_scaled_right = scaled_right + expand_width
+        back_scaled_bottom = scaled_bottom + expand_bottom
+        back_scaled_left = scaled_left - expand_width
+
+
+        back_scaled_top = int(back_scaled_top*image_height)
+        back_scaled_right = int(back_scaled_right*image_width)
+        back_scaled_bottom = int(back_scaled_bottom*image_height)
+        back_scaled_left = int(back_scaled_left*image_width)
+
+#         print(back_scaled_top, back_scaled_right, back_scaled_left ,back_scaled_left)
+
+#         face_image = image[back_scaled_top:back_scaled_bottom,
+#                            back_scaled_left:back_scaled_right]
+    # return center_x*image_width, center_y*image_height
+    return back_scaled_top, back_scaled_right, back_scaled_bottom, back_scaled_left
+
+
+import argparse
+import os
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip
+from pydub.utils import mediainfo
+
+
+os.makedirs("./output", exist_ok=True)
+
+parser = argparse.ArgumentParser(description="This is a source video cropper.")
+
+parser.add_argument('--video', dest='video_input', type=str, required=True, help='input video file path to be cropped.')
+parser.add_argument('--audio', dest='audio_input', type=str, default='./output/tts_audio.wav', help='input audio file for the video. it decides the length of the video.')
+parser.add_argument('--output', dest='video_output', type=str, default='./output/cropped_video.mp4', help='output file path of the cropped video.')
+parser.add_argument('--fps', dest='video_fps', type=int, default=30, help='fps of the cropped video.')
+parser.add_argument('--resolution', dest='video_res', type=int, default=256, help='resolution x resolution, 256 for vox.')
+
+args = parser.parse_args()
+
+def main():
+    audio_info = mediainfo(args.audio_input)
+    audio_duration = float(audio_info['duration'])
+
+    # Write the video clip
+    video_clip = VideoFileClip(args.video_input).subclip(0, audio_duration)
+    modified_clip = video_clip.set_fps(args.video_fps) # set fps == 30
+    # modified_clip.write_videofile(args.video_output)
+
+    # Capture the first frame
+    first_frame = modified_clip.get_frame(0)
+
+    # back_scaled_top, back_scaled_right, back_scaled_bottom, back_scaled_left
+    crop_y1, crop_x2, crop_y2, crop_x1 = frame_definer(first_frame)
+
+    # crop and resize
+    cooked_video = modified_clip.crop(x1=crop_x1, x2=crop_x2, y1=crop_y1, y2=crop_y2)
+
+    if os.path.exists(args.video_output):
+        os.remove(args.video_output)
+    cooked_video.write_videofile(args.video_output, temp_audiofile=False)
+
+if __name__ == '__main__':
+	main()
